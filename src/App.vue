7b982f11abd4e49e007bc62460b120c8d699f8bc
henser <hserro@gmail.com>
2019-05-20 17:27:04 +0100

    Removed unused dependencies;

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,35 @@
 <template>
   <div id="app">
-    <Kittens msg="meow!"/>
+    <Header/>
+    <Kittens />
   </div>
 </template>
 
 <script>
-import Kittens from './components/Kittens.vue'
+import Header from './components/Header'
+import Kittens from './components/Kittens'
 
 export default {
   name: 'app',
   components: {
+    Header,
     Kittens
   }
 }
 </script>
 
 <style>
+
+html,
+body {
+  background-color: #fafafa;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
